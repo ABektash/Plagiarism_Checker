dbbfa5130f2afe41b52f1879d450491aeb820ed3
Youssef-Abdelshahid <youssefmoshahid@gmail.com>
2024-10-20 21:37:58 +0300

    Created the navbar and sidebar for the admin

---
+++ App/Views/adminDashboard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Boxicons -->
+	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+	<!-- My CSS -->
+	<link rel="stylesheet" href="/Plagiarism_Checker/public/assets/css/admin.css">
+
+	<title>Plagiarism Detection</title>
+</head>
+
+<body>
+
+
+	<!-- SIDEBAR -->
+	<?php include 'inc/sidebar.php'; ?>
+	<!-- SIDEBAR -->
+
+
+
+	<!-- CONTENT -->
+	<section id="content">
+		<!-- NAVBAR -->
+		<?php include 'inc/navbar.php'; ?>
+		<!-- NAVBAR -->
+
+		<!-- MAIN -->
+		<main>
+			<div class="head-title">
+				<div class="left">
+					<h1>Dashboard</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li><i class='bx bx-chevron-right'></i></li>
+						<li>
+							<a class="active" href="#">Home</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</main>
+		<!-- MAIN -->
+	</section>
+	<!-- CONTENT -->
+	
+</body>
+
+</html>
