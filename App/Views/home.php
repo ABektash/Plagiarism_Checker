dc15c1fa52f06ad9e593339413989eee0c40a0ba
Youssef-Abdelshahid <youssefmoshahid@gmail.com>
2024-10-22 00:54:50 +0300

    Added images for the home page

--- App/Views/home.php
+++ App/Views/home.php
@@ -20,25 +20,25 @@
                     title: "Student Account",
                     description: "Create and manage your student account to submit assignments for plagiarism analysis. Track your submissions and view detailed reports to improve your writing.",
                     buttonText: "Sign Up",
-                    backgroundImage: "/Plagiarism_Checker/public/assets/images/image.png"
+                    backgroundImage: "/Plagiarism_Checker/public/assets/images/yy22.jpg"
                 },
                 {
                     title: "Instructor Account",
                     description: "Manage your instructor account to review submissions, access plagiarism reports, and guide students on improving their originality and writing skills.",
                     buttonText: "Get Started",
-                    backgroundImage: "/Plagiarism_Checker/public/assets/images/image.png"
+                    backgroundImage: "/Plagiarism_Checker/public/assets/images/yy33.jpg"
                 },
                 {
                     title: "Plagiarism Detection",
                     description: "Our advanced plagiarism detection algorithms ensure that submissions are thoroughly analyzed against a vast database of sources to detect copied content.",
                     buttonText: "Check Now",
-                    backgroundImage: "/Plagiarism_Checker/public/assets/images/image.png"
+                    backgroundImage: "/Plagiarism_Checker/public/assets/images/yy4.jpg"
                 },
                 {
                     title: "Discussion Forum",
                     description: "Join the discussion forum to collaborate with peers, ask questions, and seek advice on how to maintain originality and improve writing quality.",
                     buttonText: "Join the Forum",
-                    backgroundImage: "/Plagiarism_Checker/public/assets/images/image.png"
+                    backgroundImage: "/Plagiarism_Checker/public/assets/images/yy5.jpg"
                 }
             ];
 
